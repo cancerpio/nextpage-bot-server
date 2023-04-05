2577fa0afcfad233349a000903b008ce3b43a10e
Cancerpio <breezesky627@gmail.com>
2023-04-05 17:56:43 +0800

    [master]  initial project

---
+++ src/main/java/com/cancerpio/nextpagelinebotserver/NextpageLineBotServerApplication.java
@@ -0,0 +1,13 @@
+package com.cancerpio.nextpagelinebotserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextpageLineBotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextpageLineBotServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cancerpio/nextpagelinebotserver/NextpageLineBotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cancerpio.nextpagelinebotserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextpageLineBotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

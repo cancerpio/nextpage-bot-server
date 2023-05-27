3dfe4ab127d6724c7c3d06b284673f7a576da3d6
Cancerpio <breezesky627@gmail.com>
2023-05-27 14:44:58 +0800

    [format及專案相關]  - 調整code format, 將InteliJ IDE需要的設定檔案加入

--- src/main/java/com/cancerpio/nextpagelinebotserver/NextpageLineBotServerApplication.java
+++ src/main/java/com/cancerpio/nextpagelinebotserver/NextpageLineBotServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NextpageLineBotServerApplication {
 
     public static void main(String[] args) {
-	SpringApplication.run(NextpageLineBotServerApplication.class, args);
+        SpringApplication.run(NextpageLineBotServerApplication.class, args);
     }
 
 }
